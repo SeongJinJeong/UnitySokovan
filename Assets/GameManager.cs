2e609fc60730a135795a6f73dd5f47dcb03ed82e
SeongJin Jeong <jsj0718tjdwl@naver.com>
2023-06-28 23:25:26 +0900

    New Stage

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,13 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
     public GameObject winUI;
-    public ItemBox[] itemBoxes;
     public bool isGameOver = false;
-    // Start is called before the first frame update
+
+    private int currentStage = 1;
+    private ItemBox[] itemBoxes;
+
+    private void Awake()
+    {
+        if(instance == null)
+            instance = this;
+
+        if(this != instance)
+            Destroy(gameObject);
+    }
     void Start()
     {
+        this.ResetData();
+    }
+    private void ResetData()
+    {
         this.isGameOver = false;
+        GameObject[] itemBoxes = GameObject.FindGameObjectsWithTag("ItemBox");
+        this.itemBoxes = new ItemBox[itemBoxes.Length];
+        for (int i = 0; i < this.itemBoxes.Length; i++)
+        {
+            this.itemBoxes[i] = itemBoxes[i].GetComponent<ItemBox>();
+        }
     }
 
     // Update is called once per frame
@@ -19,7 +40,13 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            SceneManager.LoadScene("SampleScene");
+            if(this.isGameOver)
+            {
+                this.onGameClear();
+            } else
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
             return;
         }
 
@@ -38,5 +65,12 @@ public class GameManager : MonoBehaviour
             isGameOver = true;
             winUI.SetActive(true);
         }
+    }
+
+    public void onGameClear()
+    {
+        this.currentStage++;
+        this.isGameOver = false;
+        SceneManager.LoadScene("Scene" + this.currentStage);
     }
 }
